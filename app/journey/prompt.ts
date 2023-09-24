aa250c76f96d32065465f44be26e8bf6e867f466
Kyle Goben <kygoben@gmail.com>
2023-09-24 10:56:08 -0500

    new topics

--- app/journey/prompt.ts
+++ app/journey/prompt.ts
@@ -3,15 +3,164 @@ import { TreeNode, TreeDTO, ExpandTreeDTO } from "@/types/data";
 
 // Define the API endpoint
 const API_ENDPOINT = "/api/chat";
-const Topics = [
-  "Deductibles",
-  "Premiums",
-  "Coverage",
-  "Liability",
-  "Collision",
-  "Comprehensive",
+const AutoTopics = [
+  "Premium",
+  "Deductible",
+  "Policyholder",
+  "Claim",
+  "Liability Coverage",
+  "Collision Coverage",
+  "Comprehensive Coverage",
   "Uninsured/Underinsured Motorist Coverage",
-];
+  "Medical Payments Coverage",
+  "Personal Injury Protection (PIP)",
+  "Bodily Injury Liability",
+  "Property Damage Liability",
+  "Gap Insurance",
+  "Rental Reimbursement",
+  "Towing and Labor",
+  "Ride-Sharing Coverage",
+  "SR-22",
+  "Telematics",
+  "No-Fault Insurance",
+  "At-Fault",
+  "Actuary",
+  "Adjuster",
+  "Appraisal",
+  "Declarations Page",
+  "Exclusion",
+  "Grace Period",
+  "Indemnity",
+  "Insurable Interest",
+  "Insurance Score",
+  "Named Insured",
+  "Occasional Driver",
+  "Primary Use",
+  "Quote",
+  "Subrogation",
+  "Underwriting",
+  "Usage-Based Insurance",
+  "Agent",
+  "Broker",
+  "Claimant",
+  "Endorsement",
+  "Salvage Title",
+  "Total Loss",
+  "Accident Forgiveness",
+  "Diminishing Deductible",
+  "Roadside Assistance",
+  "Classic Car Insurance",
+  "High-Risk Auto Insurance",
+  "Non-Owners Auto Insurance",
+  "Pay-Per-Mile Insurance",
+  "Credit-Based Insurance Score",
+  "Aftermarket Parts",
+  "OEM Parts",
+  "Ride-Share Insurance"
+  ];
+
+const HomeTopics = [
+  "Dwelling Coverage",
+  "Personal Property Coverage",
+  "Liability Protection",
+  "Guest Medical Protection",
+  "Additional Living Expenses",
+  "Deductible",
+  "Actual Cash Value",
+  "Replacement Cost",
+  "Extended Replacement Cost",
+  "Policyholder",
+  "Premium",
+  "Underwriting",
+  "Peril",
+  "Exclusion",
+  "Endorsement",
+  "Rider",
+  "Floater",
+  "Binders",
+  "Claim",
+  "Liability Limit",
+  "Umbrella Policy",
+  "Earthquake Insurance",
+  "Flood Insurance",
+  "Named Peril",
+  "All-Risk/All-Peril",
+  "Scheduled Personal Property",
+  "Home Inventory",
+  "Credit Report",
+  "Depreciation",
+  "Public Adjuster",
+  "Appraisal",
+  "Market Value",
+  "Subrogation",
+  "Loss of Use",
+  "Ordinance or Law Coverage",
+  "Water Backup Coverage",
+  "Loss Assessment Coverage",
+  "High-Risk Home Insurance",
+  "Homeowners Association (HOA)",
+  "Condominium Insurance",
+  "Renters Insurance",
+  "Mobile Home Insurance",
+  "Title Insurance",
+  "Mortgage Insurance",
+  "Home Warranty",
+  "Catastrophe",
+  "Liability Coverage",
+  "Medical Payments Coverage",
+  "Personal Umbrella Policy",
+  "Vacant and Unoccupied Home Insurance"
+  ];
+
+const lifeTopics = [
+  "Premium",
+  "Beneficiary",
+  "Death Benefit",
+  "Policyholder",
+  "Insurable Interest",
+  "Underwriting",
+  "Term Life Insurance",
+  "Whole Life Insurance",
+  "Universal Life Insurance",
+  "Variable Life Insurance",
+  "Cash Value",
+  "Dividend",
+  "Rider",
+  "Exclusion",
+  "Grace Period",
+  "Surrender Value",
+  "Face Value",
+  "Convertible Term",
+  "Renewable Term",
+  "Annuity",
+  "Policy Loan",
+  "Free Look Period",
+  "Incontestability Clause",
+  "Accelerated Death Benefit",
+  "Waiver of Premium",
+  "Level Premium",
+  "Decreasing Term",
+  "Increasing Term",
+  "Permanent Life Insurance",
+  "Group Life Insurance",
+  "Credit Life Insurance",
+  "Endowment Policy",
+  "Maturity Date",
+  "Mortality Charge",
+  "Policy Reinstatement",
+  "Risk Classification",
+  "Settlement Option",
+  "Substandard Risk",
+  "Suicide Clause",
+  "Underwriter",
+  "Viatical Settlement",
+  "Living Benefits",
+  "Guaranteed Insurability"
+  ];
+
+
+
+
 
 
 
@@ -21,7 +170,19 @@ const Topics = [
  * @return Promise<OpenAIResponse> The response from the OpenAI API.
  */
 export async function createNewTree(treeData: TreeDTO): Promise<TreeNode> {
-  const randomTopic = Math.floor(Math.random() * 7);
+  let Topics = AutoTopics;
+  let randomTopic = 0;
+  if(treeData.insuranceType == "Auto") {
+    Topics = AutoTopics;
+    randomTopic = Math.floor(Math.random() * AutoTopics.length);
+  } else if(treeData.insuranceType == "Home") {
+    Topics = HomeTopics;
+    randomTopic = Math.floor(Math.random() * HomeTopics.length);
+  } else if (treeData.insuranceType == "Life") {
+    Topics = lifeTopics;
+    randomTopic = Math.floor(Math.random() * lifeTopics.length);
+  }
+  // const randomTopic = Math.floor(Math.random() * 7);
   const prompt = `
   Create a specific story senario with 2 user options in the form of a decision tree
 
@@ -68,7 +229,18 @@ export async function createNewTree(treeData: TreeDTO): Promise<TreeNode> {
  * @return Promise<OpenAIResponse> The response from the OpenAI API.
  */
 export async function expandTree(treeData: ExpandTreeDTO): Promise<TreeNode> {
-  const randomTopic = Math.floor(Math.random() * 7);
+  let Topics = AutoTopics;
+  let randomTopic = 0;
+  if(treeData.insuranceType == "Auto") {
+    Topics = AutoTopics;
+    randomTopic = Math.floor(Math.random() * AutoTopics.length);
+  } else if(treeData.insuranceType == "Home") {
+    Topics = HomeTopics;
+    randomTopic = Math.floor(Math.random() * HomeTopics.length);
+  } else if (treeData.insuranceType == "Life") {
+    Topics = lifeTopics;
+    randomTopic = Math.floor(Math.random() * lifeTopics.length);
+  }
   let prompt;
   const random = Math.random() * 3;
   if (random == 0) {
